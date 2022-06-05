f76fb0fb6539f53c86173f5f74b1d8ee3e5c1b49
OlenaLotos <olenameshanych@gmail.com>
2022-06-05 11:40:37 +0300

    edit task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,11 +1,17 @@
-// import flatpickr from "flatpickr";
-// import "flatpickr/dist/flatpickr.min.css";
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const input = document.querySelector('#datetime-picker');
-const startButton = document.querySelector('[data-start]');
+const startBtn = document.querySelector('button[data-start]');
+const timerInput = document.querySelector('input#datetime-picker');
+const timerDays = document.querySelector('.value[data-days]');
+const timerHours = document.querySelector('.value[data-hours]');
+const timerMinutes = document.querySelector('.value[data-minutes]');
+const timerSeconds = document.querySelector('.value[data-seconds]');
 
-const currentDate = Date.now();
+const currentTime = new Date();
+
+startBtn.disabled = true;
 
 const options = {
   enableTime: true,
@@ -13,56 +19,59 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-      if (selectedDates[0] < currentDate) {
-        Notify.failure('Please choose a date in the future', {
-        position: 'center-top',
-      });
-    //   window.alert('Please choose a date in the future');
-        
+      if (selectedDates[0] <= currentTime) {
+        window.alert('Please choose a date in the  future');
+    //   Notify.warning('Please choose a date in the  future');
     } else {
-      startButton.disabled = false;
-      startButton.addEventListener('click', timer(selectedDates[0]));
+      startBtn.disabled = false;
     }
+ 
   },
 };
+startBtn.addEventListener('click', onBtnClick);
+flatpickr(timerInput, options);
 
-flatpickr(input, options);
+let timerId = null;
+function onBtnClick() {
+    const inputDate = new Date(timerInput.value);
+  
+    startBtn.disabled = true;
+
+    timerId = setInterval(() => {
+        const timeSubtraction = inputDate - new Date();
+        const time = convertMs(timeSubtraction);
+        console.log(convertMs(timeSubtraction));
+        updateTime(time);
+        if (timeSubtraction < 1000) {
+            clearInterval(timerId);
+            Notify.info('It is a time!!!');
+        }
+    }, 1000);
+};
+ 
+function updateTime({ days, hours, minutes, seconds }) {
+  timerDays.textContent = days;
+  timerHours.textContent = hours;
+  timerMinutes.textContent = minutes;
+  timerSeconds.textContent = seconds;
+}
+
+startBtn.disabled = true;
+
+function addCorrectLength(value) {
+  return String(value).padStart(2, '0');
+}
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day).padStart(2, '0');
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour).padStart(2, '0');
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute).padStart(2, '0');
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second).padStart(2, '0');
+  const days = addCorrectLength(Math.floor(ms / day));
+  const hours = addCorrectLength(Math.floor((ms % day) / hour));
+  const minutes = addCorrectLength(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addCorrectLength(Math.floor((((ms % day) % hour) % minute) / second));
 
-    console.log(
-    `days: ${days}, hours: ${hours}, minutes: ${minutes}, seconds: ${seconds}`
-  );
   return { days, hours, minutes, seconds };
 }
-
-const showTime = (days, hours, minutes, seconds) => {
-  document.querySelector('span[data-days]').textContent = days;
-  document.querySelector('span[data-hours]').textContent = hours;
-  document.querySelector('span[data-minutes]').textContent = minutes;
-  document.querySelector('span[data-seconds]').textContent = seconds;
-};
-
-const timer = selectedDate => {
-  setInterval(() => {
-    const ms = selectedDate - new Date();
-    let result = convertMs(ms);
-    showTime(result.days, result.hours, result.minutes, result.seconds);
-  }, 1000);
-};
-
